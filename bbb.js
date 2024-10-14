28608ee6e188108b3684738aa953002e87ae9c5c
baidu123987 <gongxi910@gmail.com>
2024-10-14 15:18:53 +0700

    bbb.js

---
+++ bbb.js
@@ -0,0 +1,13 @@
+new Vue({
+            el: '#app',
+            data() {
+                return {
+                    list: [
+                        { id: 1, img: 'https://imgsrc.baidu.com/forum/pic/item/1ad5ad6eddc451da6aa19ec8f0fd5266d01632ba.jpg', href: 'https://ml102.yfahx.cn/35.html', title: ' 1' },
+                        { id: 2, img: 'https://imgsrc.baidu.com/forum/pic/item/a686c9177f3e6709194fd7dc7dc79f3df8dc5542.jpg', href: 'https://ml102.yfahx.cn/35.html', title: ' 2', extra: 'alert-danger' },
+                        { id: 3, img: 'https://imgsrc.baidu.com/forum/pic/item/d043ad4bd11373f08377fdbce20f4bfbfbed044c.jpg', href: 'https://ml102.yfahx.cn/35.html', title: ' 3' }
+                    ],
+					
+                };
+            }
+        });
